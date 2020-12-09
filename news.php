b8715c8d87fb5fc94d789a407bf47251999eca7f
Mdsujansarkar <sujansarkar786@gmail.com>
2020-12-09 15:33:08 +0600

    test commit

--- news.php
+++ news.php
@@ -12,7 +12,6 @@
       <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
       <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
       <link href="lib/animate/animate.min.css" rel="stylesheet">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css" />
       <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
       <link rel="stylesheet" href="css/lc_lightbox.css" />
       <link rel="stylesheet" href="skins/minimal.css" />
@@ -80,46 +79,6 @@
                            </div>
                         </div>
                      </div>
-                     <div class="carousel-item-a intro-item bg-image" style="background-image: url(images/slider/poro-2.jpg)">
-                        <div class="overlay overlay-a"></div>
-                        <div class="intro-content display-table">
-                           <div class="table-cell">
-                              <div class="silder-fotter">
-                                 <div class="container">
-                                    <div class="row">
-                                       <div class="col-lg-12">
-                                          <div class="intro-body">
-                                             <p class="intro-title-top"><a href="">
-                                                </a>
-                                             </p>
-                                          </div>
-                                       </div>
-                                    </div>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                     <div class="carousel-item-a intro-item bg-image" style="background-image: url(images/slider/poros-3.jpg)">
-                        <div class="overlay overlay-a"></div>
-                        <div class="intro-content display-table">
-                           <div class="table-cell">
-                              <div class="silder-fotter">
-                                 <div class="container">
-                                    <div class="row">
-                                       <div class="col-lg-12">
-                                          <div class="intro-body">
-                                             <p class="intro-title-top"><a href="">
-                                                </a>
-                                             </p>
-                                          </div>
-                                       </div>
-                                    </div>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
                   </div>
                   <!--/ Carousel end /-->
                </div>
@@ -153,9 +112,7 @@
                            <p class="content-d color-text-a">
                               Sheikh Fazle Shams Parash is a well-known professor in English at the Brac University in Dhaka who turned into a politician holding the position of chairman of Bangladesh Awami Jubo League in 2019. The Jubo league faces crises across the country after spreading out the corruption of some top Jubo league leaders.
                            </p>
-                           </p>
                         </div>
-                        <a href="#" class="myButton">Read More</a>
                      </div>
                   </div>
                </div>
@@ -163,192 +120,7 @@
          </div>
       </section>
       <!--/ Agent Single End /-->
-      <!--/ Video Star /-->
-      <section class="section-services">
-         <div class="container-fluid">
-         <div class="row">
-            <div class="col-md-12">
-               <div class="context">
-                  <h1>VISION 2021-2041</h1>
-                  <p>Prime Minister Sheikh Hasina, with the help of her ICT Affairs Advisor Sajeeb Wazed, unveiled the Awami League's 'Digital Bangladesh' aspiration in 2008. The plan was to transform Bangladesh into a technologically advanced nation by the year 2021. For the First time, such an ambitious vision was incorporated into the electoral manifesto of any political party. This vision was put into motion right after the Awami League formed the Government following a landslide electoral victory in December 2008.</p>
-               </div>
-               <div class="area" >
-                  <ul class="circles">
-                     <li></li>
-                     <li></li>
-                     <li></li>
-                     <li></li>
-                     <li></li>
-                     <li></li>
-                     <li></li>
-                     <li></li>
-                     <li></li>
-                     <li></li>
-                  </ul>
-               </div >
-            </div>
-         </div>
-      </section>
-      <section class="agent-single" style="background-image:url('images/background/watercolor.png')">
-         <div class="container">
-         <div class="row">
-            <div class="col-sm-12">
-               <div class="row">
-                  <div class="col-md-7 section-md-t3">
-                     <div class="agent-info-box1 agent-info-box2">
-                        <div class="agent-contents mb-3"  style="padding:20px 0">
-                           <h3>Mujib Borsho Celebration</h3>
-                           <p class="content-d color-text-a">
-                              The government of Bangladesh has announced the commemoration of 2020–2021 as the Mujib Year (Bengali: মুজিব বর্ষ) on the occasion of the centennial birth anniversary of the founding leader of the country, Sheikh Mujibur Rahman
-                           </p>
-                           </p>
-                        </div>
-                        <a href="https://en.wikipedia.org/wiki/Mujib_Year" target="_blank" class="myButton">Read More</a>
-                     </div>
-                  </div>
-                  <div class="col-md-5">
-                     <div class="mujib-image">
-                        <img src="images/favcon/row-image-Mujib100.png">
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </section>
-      <!-- Video -->
-      <section class="agent-single" style="background-image:url('images/background/leaves.png')">
-         <div class="container">
-         <div class="row">
-            <div class="col-md-12">
-               <div class="main-heading">
-                  <h1 class="text-center">Video Gallery</h1>
-                  <p class="text-center">Our great moment Our great momentOur great momentOur great momentOur great momentOur great momentOur great momentOur great momentOur great momentOur great momentOur great momentOur great momentOur great momentOur great momentOur great momentOur great momentOur great moment</p>
-               </div>
-            </div>
-            <div class="col-md-4">
-               <a data-fancybox href="https://www.youtube.com/watch?v=6QtN4TL44bk">
-                  
-                  <div class="video-thumbnail">
-                     <img src="images/youtube_image/you_one.png" width="100%" height="150px">
-                  </div>
-               </a>
-            </div>
-            <div class="col-md-4">
-               <a data-fancybox href="https://www.youtube.com/watch?v=mCH_PzcOcoE">
-                  
-                  <div class="video-thumbnail">
-                     <img src="images/youtube_image/you_two.png" width="100%" height="150px">
-                  </div>
-               </a>
-            </div>
-            <div class="col-md-4">
-               <a data-fancybox href="https://www.youtube.com/watch?v=W_pW--KYEfo">
-                  
-                  <div class="video-thumbnail">
-                     <img src="images/youtube_image/you_three.png" width="100%" height="150px">
-                  </div>
-               </a>
-            </div>
-            <div class="col-md-4">
-               <a data-fancybox href="https://www.youtube.com/watch?v=oGR5jRfrOVQ">
-                  
-                  <div class="video-thumbnail">
-                     <img src="images/youtube_image/you_four.png" width="100%" height="150px">
-                  </div>
-               </a>
-            </div>
-            <div class="col-md-4">
-               <a data-fancybox href="https://www.youtube.com/watch?v=csyFkmMWIV8">
-                  
-                  <div class="video-thumbnail">
-                     <img src="images/youtube_image/you_six.png" width="100%" height="150px">
-                  </div>
-               </a>
-            </div>
-            <div class="col-md-4">
-               <a data-fancybox href="https://www.youtube.com/watch?v=6QtN4TL44bk">
-                  
-                  <div class="video-thumbnail">
-                     <img src="images/youtube_image/you_five.png" width="100%" height="150px">
-                  </div>
-               </a>
-            </div>
-         </div>
-      </section>
-      <!-- image gallery --> 
-      <section id="work">
-         <div class="content-box">
-            <div class="content-title wow bounceInUp" data-wow-duration="2s" data-wow-delay=".5s">
-               <h1 class="text-center">GALLERY</h1>
-               <p class="text-center middle">Our great moment Our great momentOur great momentOur great momentOur great momentOur great momentOur great momentOur great momentOur great momentOur great momentOur great momentOur great momentOur great momentOur great momentOur great momentOur great momentOur great moment</p>
-               <div class="content-title-underline-heading"></div>
-            </div>
-         </div>
-         <div class="container-fluid">
-            <div class="row no-mergin" id="lcl_elems_wrapper">
-               <div class="col-md-3">
-                  <div class="image-wrapper">
-                     <a href="images/gallery/image.png">
-                     <img src="images/gallery/image.png" alt="" class="img-responsive" />
-                     </a>
-                     
-                  </div>
-               </div>
-               <div class="col-md-3">
-                  <div class="image-wrapper">
-                     <a href="images/gallery/image2.jpg">
-                     <img src="images/gallery/image2.jpg" alt="" class="img-responsive">
-                     </a>
-                  </div>
-               </div>
-               <div class="col-md-3">
-                  <div class="image-wrapper">
-                     <a href="images/gallery/image4.jpg">
-                     <img src="images/gallery/image4.jpg" alt="" class="img-responsive">
-                     </a>
-                  </div>
-               </div>
-               <div class="col-md-3">
-                  <div class="image-wrapper">
-                     <a href="images/gallery/image5.jpg">
-                     <img src="images/gallery/image5.jpg" alt="" class="img-responsive">
-                     </a>
-                  </div>
-               </div>
-               <div class="col-md-3">
-                  <div class="image-wrapper">
-                     <a href="images/gallery/image6.png">
-                     <img src="images/gallery/image6.png" alt="" class="img-responsive">
-                     </a>
-                  </div>
-               </div>
-               <div class="col-md-3">
-                  <div class="image-wrapper">
-                     <a href="images/gallery/image7.jpg">
-                     <img src="images/gallery/image7.jpg" alt="" class="img-responsive">
-                     </a>
-                  </div>
-               </div>
-               <div class="col-md-3">
-                  <div class="image-wrapper">
-                     <a href="images/gallery/image8.jpg">
-                     <img src="images/gallery/image8.jpg" alt="" class="img-responsive">
-                     </a>
-                  </div>
-               </div>
-               <div class="col-md-3">
-                  <div class="image-wrapper">
-                     <a href="images/gallery/image9.jpg">
-                     <img src="images/gallery/image9.jpg" alt="" class="img-responsive">
-                     </a>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </section>
-      <!-- Video -->
-      <!-- Video -->
-      <section class="agent-single" style="background-image:url('images/background/leaves.png')">
+       <section class="agent-single" style="background-image:url('images/background/leaves.png')">
          <div class="container">
          <div class="row">
             <div class="col-md-12">
@@ -435,12 +207,17 @@
          </div>
       </section>
         <!-- Site footer -->
+      <!-- image gallery --> 
+      
+      <!-- Video -->
+
+        <!-- Site footer -->
     <footer class="site-footer">
       <div class="container">
         <div class="row">
           <div class="col-sm-12 col-md-6">
             <h6>About</h6>
-            <p class="text-justify">Sheikh Fazle Shams Parash is a well-known professor in English at the Brac University in Dhaka who turned into a politician holding the position of chairman of Bangladesh Awami Jubo League in 2019. The Jubo league faces crises across the country after spreading out the corruption of some top Jubo league leaders.</p>
+            <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
           </div>
 
           <div class="col-xs-6 col-md-3">
@@ -472,7 +249,7 @@
         <div class="row">
           <div class="col-md-8 col-sm-6 col-xs-12">
             <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
-         <a href="#">#</a>.
+         <a href="#">Scanfcode</a>.
             </p>
           </div>
 
@@ -503,7 +280,6 @@
       </script>
       <script src="js/main.js"></script>
       <script src="js/lc_lightbox.lite.js" type="text/javascript"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
       <script type='text/javascript'>
          $(document).ready(function() {
              var $obj = lc_lightbox('#lcl_elems_wrapper a');
--- publication.php
+++ publication.php
@@ -120,62 +120,20 @@
          </div>
       </section>
       <!--/ Agent Single End /-->
-      <section class="agent-single" style="background-image:url('images/background/watercolor.png')">
-         <div class="container">
-         <div class="row">
-            <div class="col-sm-12">
-               <div class="row">
-                  <div class="col-md-6 section-md-t3">
-                     <div class="agent-info-box1 agent-info-box2">
-                        <div class="agent-contents mb-3"  style="padding:20px 0">
-                           <h3>ADJUNCT FACULTY of ENGLISH LANGUAGE & LITERATURE</h3>
-                           <p class="content-d color-text-a">
-                              TESOL Certified (Ontario) professional teacher of English language and Literature. Innovative and devoted scholar of English composition and literature, offering ten years of teaching experience to university undergraduates and mature professional learners. Possess thorough and methodological research abilities, demonstrated by publication of three research papers in peer-reviewed scholarly journals. Notably presented on academic writing at the globally recognized EATAW conference in UK. <br />
-                              Characterized as a skilled educational administrator, facilitator, and academic coordinator with demonstrated expertise in curriculum design and development. Scholar in the field of composition, marked by rapid growth and involvement in facilitating and mentoring. 
-                           </p>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="col-md-6">
-                     <div class="agent-info-box1 agent-info-box2">
-                        <div class="agent-contents mb-3"  style="padding:20px 0">
-                           <h3>ACADEMIC ACHIEVEMENTS</h3>
-                           <div class="list-group" style="background-color: transparent;">
-                                <button type="button" class="list-group-item list-group-item-action" >
-                                  <b>Teaching English to Speakers of Other Languages (TESOL) Certified (2017)</b> |<i>York University</i>
-                                </button>
-                                <button type="button" class="list-group-item list-group-item-action" >
-                                  <b>Master of Arts Degree, English Literature</b> |<i>Colorado State University GPA: 3.7/4.0 | Ann Zimdahl Memorial Scholar</i>
-                                </button>
-                                <button type="button" class="list-group-item list-group-item-action" >
-                                  <b>Special Graduate Studies in English</b> |<i>Brown University, Rhode Island</i>
-                                </button>
-                                <button type="button" class="list-group-item list-group-item-action" >
-                                  <b>Master of Arts, English Literature</b> |<i>University of Dhaka, Bangladesh</i>
-                                </button>
-                                <button type="button" class="list-group-item list-group-item-action" >
-                                  <b>Bachelor of Arts</b> |<i>National University, Bangladesh</i>
-                                </button>
-                        </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </section>
       <!-- Video -->
       <section class="agent-single" style="background-image:url('images/background/leaves.png')">
          <div class="container">
          <div class="row">
             <div class="col-md-12">
                <div class="main-heading">
-                  <h4 class="text-center" style="text-transform: capitalize;"><b>Thesis:</b> Franz Boas and the Postcolonial Debate on Representation</h4>
+                  <h4 class="text-center" style="text-transform: capitalize;">PUBLICATIONS & PRESENTATIONS</h4>
                   <ul>
-                     <li >Studied Jean Rhys’ Wide Sargasso Sea to explore its misrepresentation and exploitation of the cultural ‘Other.’ </li>
-                     <li >Reached an alternative understanding of the cultural ‘Other’ from the heavy-handed clutch of Edward Said’s Orientalism. Showed that by reclaiming an alternative understanding of these less powerful groups of people, their identity can be reconstructed. </li>
-                     <li >Conducted interdisciplinary research to use Franz Boas’ anthropological analytical tools to understand the 'Other' from their local contexts, rather than from the outside.</li>
-                      <li >19th & 20th Century Survey of American Literature | Reading Literary Recent Theory Research Methods & Theory | Medieval Epic & Romance | Technical Writing Imperial Fictions & Postcolonial Ventures | Reading the Harlem Renaissance </li>
+                     <li >Shams S.F. (2018). <i>A Paradox of Assimilation: Is Wes a misfit in Schumann’s Acculturation Model? </i>Rethinking Disciplinary Diversity: Challenges of Teaching English in the 21st Century. A Two-Day International Conference. BRAC University, Bangladesh. </li>
+                     <li >Shams S.F. (2011). “Reaching for the Other across the Wide Sargasso Sea.” <i>East West Journal of Humanities,</i> Vol-2 No-2, 19-34.</li>
+                     <li >Shams S.F. (2010). “Book Review: Taslima Nasrin’s Lazza <i>(Shame).” Spectrum,</i> Journal of English Department, University of Dhaka Vol-6 & 7, 235-238. </li>
+                      <li >Shams S.F. (2009). <i>Practical Proposal as Target Genre: Teaching Critical Thinking in Bangladesh</i>. European Association for the Teaching of Academic Writing Conference (EATAW), Coventry University. </li>
+                      <li >Shams S.F. (2008).<i> Subversive Images of Women in Medieval English Literature: A Selective Reading.</i> BRAC University Journal, Vol-5, No-2, 105-111.</li>
+                      <li >Shams S.F. (2002). <i>The Contributions of Franz Boas in the Literature of the Harlem Renaissance.</i> The First Annual Literary Conference in Literary Criticism, English Department, Colorado State University.</li>
                   </ul>
                </div>
             </div>
